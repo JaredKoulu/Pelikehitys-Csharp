b6f7bb061a3459206bef186112b5d8220bd1538e
jared <jared.koulu@gmail.com>
2026-01-19 10:33:42 +0200

    Ovi tehtävä

---
+++ Tehtävä 1 - Ovi/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Tehtävä_1___Ovi
+{
+    internal class Program
+    {
+        enum oventila
+        {
+            Auki,
+            Kiinni,
+            Lukittu
+        }
+        static void Main(string[] args)
+        {
+            
+            oventila tila = oventila.Auki;
+            while (true)
+            {
+                Console.WriteLine($"Ovi on nyt {tila}");
+                Console.WriteLine("Mitä haluat tehdä?");
+                string valinta = Console.ReadLine();
+                if (valinta == "avaa" && tila == oventila.Kiinni)
+                {   
+                    tila = oventila.Auki;
+                }
+                else if (valinta == "sulje" && tila == oventila.Auki)
+                {
+                    tila = oventila.Kiinni;
+                }
+                else if (valinta == "lukitse" && tila == oventila.Kiinni)
+                {
+                    tila = oventila.Lukittu;
+                }
+                else if (valinta == "avaa lukko" && tila == oventila.Lukittu)
+                { 
+                    tila = oventila.Kiinni;
+                }
+            }
+        }
+    }
+}
