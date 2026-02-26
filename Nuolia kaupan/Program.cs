a3afd92436229b2b0b27ec789470d0fb8355317f
jared <jared.koulu@gmail.com>
2026-02-26 12:42:22 +0200

    tehtävä 3 valmis

--- Nuolia kaupan/Program.cs
+++ Nuolia kaupan/Program.cs
@@ -14,73 +14,98 @@
             kanansulka, // 1
             kotkansulka // 5
         }
-        enum pituus
-        {
-            pituus, // 60-100 cm
-        }
-
         class Nuoli
         {
             public kärki Kärki { get; set; }
             public perä Perä { get; set; }
-            public pituus Pituus { get; set; }
-            public Nuoli(kärki kärki, perä perä, pituus pituus)
+            public Nuoli(kärki kärki, perä perä)
             {
                 this.Kärki = kärki;
                 this.Perä = perä;
-                this.Pituus = pituus;
+            }
+
+            public int LaskeHinta()
+            {
+                int hinta = 0;
+                switch (Kärki)
+                {
+                    case kärki.puu:
+                        hinta += 3;
+                        break;
+                    case kärki.teräs:
+                        hinta += 5;
+                        break;
+                    case kärki.timantti:
+                        hinta += 50;
+                        break;
+                }
+                switch (Perä)
+                {
+                    case perä.lehti:
+                        hinta += 0;
+                        break;
+                    case perä.kanansulka:
+                        hinta += 1;
+                        break;
+                    case perä.kotkansulka:
+                        hinta += 5;
+                        break;
+                }
+                Console.WriteLine("Valitse nuolen pituus (60-100 cm): ");
+                if (hinta > 0)
+                {
+                    int pituusArvo = int.Parse(Console.ReadLine());
+                    if (pituusArvo >= 60 && pituusArvo <= 100)
+                    {
+                        hinta += (int)(pituusArvo * 0.05);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Virheellinen pituus, nuolen hinta ei muutu.");
+                    }
+                }
+                return hinta;
             }
         }
         static void Main(string[] args)
         {
-            int hinta = 0;
-
             Console.WriteLine("Valitse nuolen kärki: puu, teräs, timantti");
             string kärkiValinta = Console.ReadLine();
-            if (kärkiValinta == "puu")
-            {
-                hinta += 3;
-            }
-            else if (kärkiValinta == "teräs")
-            {
-                hinta += 5;
-            }
-            else if (kärkiValinta == "timantti")
-            {
-                hinta += 50;
-            }
+            kärki valittuKärki = KärkiString(kärkiValinta);
 
             Console.WriteLine("Valitse nuolen perä: lehti, kanansulka, kotkansulka");
             string peräValinta = Console.ReadLine();
-            if (peräValinta == "lehti")
-            {
-                hinta += 0;
-            }
-            else if (peräValinta == "kanansulka")
-            {
-                hinta += 1;
-            }
-            else if (peräValinta == "kotkansulka")
-            {
-                hinta += 5;
-            }
+            perä valittuPerä = PeräString(peräValinta);
 
-            Console.WriteLine("Valitse nuolen pituus (60-100 cm): ");
-            if (hinta > 0) 
-            {
-                int pituus = int.Parse(Console.ReadLine());
-                if (pituus >= 60 && pituus <= 100)
-                {
-                    hinta += (int)(pituus * 0.05);
-                }
-                else
-                {
-                    Console.WriteLine("Virheellinen pituus, nuolen hinta ei muutu.");
-                }
-            }
-            PalautaHinta(hinta);
+            Nuoli nuoli = new Nuoli(valittuKärki, valittuPerä);
+            int hinta = nuoli.LaskeHinta();
+
+            LopullinenHinta(hinta);
         }
-        static void PalautaHinta(int hinta)
+
+        static kärki KärkiString(string kärkiValinta)
+        {
+            return kärkiValinta switch
+            {
+                "puu" => kärki.puu,
+                "teräs" => kärki.teräs,
+                "timantti" => kärki.timantti,
+                _ => kärki.puu
+            };
+        }
+
+        static perä PeräString(string peräValinta)
+        {
+            return peräValinta switch
+            {
+                "lehti" => perä.lehti,
+                "kanansulka" => perä.kanansulka,
+                "kotkansulka" => perä.kotkansulka,
+                _ => perä.lehti
+            };
+        }
+
+        static void LopullinenHinta(int hinta)
         {
             Console.WriteLine($"Nuolen hinta on: {hinta} kolikkoa");
         }
