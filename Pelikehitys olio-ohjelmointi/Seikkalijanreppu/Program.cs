93d2cb31e5138ad8d8c88d8e829d8f888d6d7d2f
jared <jared.koulu@gmail.com>
2026-04-02 12:56:22 +0300

    tehtävä 4 alotus

---
+++ Pelikehitys olio-ohjelmointi/Seikkalijanreppu/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Seikkalijanreppu
+{
+    internal class Program
+    {
+        public class Tavara
+        {
+
+        }
+        public class Nuoli : Tavara
+        {
+        }
+        public class Jousi : Tavara
+        {
+        }
+        public class Köysi : Tavara
+        {
+        }
+        public class Vesi : Tavara
+        {
+        }
+        public class Ruoka : Tavara
+        {
+        }
+        public class Miekka : Tavara
+        {
+        }
+        public class Reppu
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Repussasi on tällähetkellä: 0/10 tavaraa, 0/30 painoa ja 0/20 tilavuus.");
+            Console.WriteLine("Mitä haluat lisätä?");
+            Console.WriteLine("1. Nuoli");
+            Console.WriteLine("2. Jousi");
+            Console.WriteLine("3. Köysi");
+            Console.WriteLine("4. Vesi");
+            Console.WriteLine("5. Ruoka");
+            Console.WriteLine("6. Miekka");
+            Console.ReadLine();
+        }
+        
+    }
+}
